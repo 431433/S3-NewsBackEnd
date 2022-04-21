10df7b5a708c6e902f0c0bdb459fb48c6aad6bc7
431433 <77282414+431433@users.noreply.github.com>
2022-04-21 10:16:56 +0200

    Add project files.

---
+++ WebAPITest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAPITest.Models;
+
+namespace WebAPITest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult News()
+        {
+            News news = new();
+            news.GetNews();
+
+            List<News> newslist = new();
+            foreach (var item in news.GetNews())
+            {
+                newslist.Add(new News(item));
+            }
+            return View(newslist);
+        }
+    }
+}
---
+++ WebAPITest/Models/News.cs
@@ -0,0 +1,68 @@
+﻿using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+
+namespace WebAPITest.Models
+{
+    public class News
+    {
+        public int Amount;
+        public string? Title;
+        public string? SourceName;
+        public string? Description;
+        public string? Url;
+        public DateTime? ReleaseDate;
+
+        public List<Article> newslist = new();
+
+        public News()
+        {}
+
+        public News(object item)
+        {
+            Item = item;
+        }
+        public News(Article news)
+        {
+            Title = news.Title;
+            SourceName = news.Source.Name;
+            Description = news.Description;
+            ReleaseDate = news.PublishedAt;
+            Url = news.Url;
+        }
+
+        public object Item { get; }
+
+        public List<Article> GetNews()
+        {
+            var newsApiClient = new NewsApiClient("64526745974a43a68186465bb4bcc8be");
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = "War",
+                SortBy = SortBys.Popularity,
+                Language = Languages.EN,
+                From = new DateTime(2022, 3, 10)
+            });
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                // total results found
+                Amount = articlesResponse.TotalResults;
+
+                foreach (var article in articlesResponse.Articles)
+                {
+                    // title
+                    Title = article.Title;
+                    //source
+                    SourceName = article.Source.Name;
+                    // description
+                    Description = article.Description;
+                    //release date
+                    ReleaseDate = article.PublishedAt;
+                    Url = article.Url;
+                    newslist.Add(article);
+                }
+            }
+            return newslist;
+        }
+    }
+}
